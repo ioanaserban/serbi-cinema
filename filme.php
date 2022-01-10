918b0c8a5c123f2d10117207c202fa3c7e55ef9b
Ioana Serban <serban.ioana29@yahoo.com>
2022-01-10 21:02:46 +0200

    film update

--- filme.php
+++ filme.php
@@ -49,7 +49,39 @@
             </tbody>
         </table>
 
-        <div class="btn btn-info my-3">Adauga un film nou</div>
+        <button type="button" class="btn btn-info my-3" data-toggle="modal" data-target="#exampleModalCenter">
+            Adauga un film nou
+        </button>
+
+        <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLongTitle">Adauga un film nou</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <form>
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Email address</label>
+                                <input type="text" class="form-control" id="filmName" aria-describedby="emailHelp" placeholder="Enter email">
+                                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputPassword1">Password</label>
+                                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                            </div>
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Inchide</button>
+                        <button type="button" class="btn btn-info">Adauga</button>
+                    </div>
+                </div>
+            </div>
+        </div>
 
     <?php } ?>
 </div>
